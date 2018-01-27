0c6d8831583840624daf34efeba80c81364f04e2
tcruzdb <32424952+tcruzdb@users.noreply.github.com>
2018-01-27 14:51:42 -0500

    Update: README.md notebooks

---
+++ notebooks/Users/tony.cruz@databricks.com/demo/azure-field-eng/click-prediction-sko/0 Info.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %md #### Click Prediction
+# MAGIC ####[Ad impressions with clicks dataset](https://www.kaggle.com/c/avazu-ctr-prediction/data)
+# MAGIC 
+# MAGIC <img src="/files/img/fraud_ml_pipeline.png" alt="workflow" width="500">
+# MAGIC 
+# MAGIC #### 1. ETL
+# MAGIC #### 2. Data Exploration / SQL
+# MAGIC #### 3. Advanced Analytics / ML
+# MAGIC 
+# MAGIC Owner: Tony / tony.cruz    
+# MAGIC Runnable: True 
+# MAGIC 
+# MAGIC **nb: the third notebook uses a GBTClassifier which takes a while to run**
+# MAGIC 
+# MAGIC Last Spark Version: 2.2
+
+# COMMAND ----------
+
